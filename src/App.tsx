936d4fb1e929ef1c070ebb33b9d0a3b0466c0647
AlgzApp <bdevelopmentwibs@gmail.com>
2026-08-22 08:02:43 +0700

    coba sava

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { db } from "./firebase";
+import {
+  collection,
+  doc,
+  setDoc,
+  deleteDoc,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "./firebase";
+import {
+  collection,
+  doc,
+  setDoc,
+  deleteDoc,
+  onSnapshot,
+} from "firebase/firestore";
 import React, { useState, useEffect } from "react";
 import {
   Calendar,
